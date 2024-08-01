14f4cb27c183691b437ac9c4a10d52a6d6aae73a
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-08-01 20:55:19 +0900

    add function to set time of alarms add function of save button

--- app/src/main/java/com/example/randomnotifier/SettingForm.kt
+++ app/src/main/java/com/example/randomnotifier/SettingForm.kt
@@ -1,14 +1,24 @@
 package com.example.randomnotifier
 
+import android.app.TimePickerDialog
 import android.widget.Button
 import android.widget.TextView
+import android.widget.TimePicker
 import androidx.appcompat.widget.SwitchCompat
 import android.os.Bundle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import android.view.View
 
 class SettingForm : AppCompatActivity() {
+    var notifyTime1Hour = DataManager.getNotifyTime1Hour()
+    var notifyTime1Min = DataManager.getNotifyTime1Minute()
+    var notifyTime2Hour = DataManager.getNotifyTime2Hour()
+    var notifyTime2Min = DataManager.getNotifyTime2Minute()
+    var notifyTime3Hour = DataManager.getNotifyTime3Hour()
+    var notifyTime3Min = DataManager.getNotifyTime3Minute()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_form)
@@ -24,25 +34,90 @@ class SettingForm : AppCompatActivity() {
         val fileNameView: TextView = findViewById(R.id.file_name_view)
         fileNameView.text = DataManager.getFilePath()
 
-        val notifyTime1Hour = DataManager.getNotifyTime1Hour()
-        val notifyTime1Min = DataManager.getNotifyTime1Minute()
         val alarm1View: TextView = findViewById(R.id.alarm1_view)
         alarm1View.text = String.format("%02d:%02d", notifyTime1Hour, notifyTime1Min)
         val alarm1Switch: SwitchCompat = findViewById(R.id.alarm1_switch)
         alarm1Switch.isChecked = DataManager.isNotifyTime1En()
 
-        val notifyTime2Hour = DataManager.getNotifyTime2Hour()
-        val notifyTime2Min = DataManager.getNotifyTime2Minute()
         val alarm2View: TextView = findViewById(R.id.alarm2_view)
         alarm2View.text = String.format("%02d:%02d", notifyTime2Hour, notifyTime2Min)
         val alarm2Switch: SwitchCompat = findViewById(R.id.alarm2_switch)
         alarm2Switch.isChecked = DataManager.isNotifyTime2En()
 
-        val notifyTime3Hour = DataManager.getNotifyTime3Hour()
-        val notifyTime3Min = DataManager.getNotifyTime3Minute()
         val alarm3View: TextView = findViewById(R.id.alarm3_view)
         alarm3View.text = String.format("%02d:%02d", notifyTime3Hour, notifyTime3Min)
         val alarm3Switch: SwitchCompat = findViewById(R.id.alarm3_switch)
         alarm3Switch.isChecked = DataManager.isNotifyTime3En()
+
+        alarm1View.setOnClickListener {
+            // 現在の時間を取得
+            val calendar = Calendar.getInstance()
+
+            // TimePickerDialogを作成して表示
+            val timePickerDialog = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+                    notifyTime1Hour = hourOfDay
+                    notifyTime1Min = minute
+                    alarm1View.text = String.format("%02d:%02d", hourOfDay, minute)
+                }
+            }, notifyTime1Hour, notifyTime1Min, true) // trueは24時間形式、falseはAM/PM形式
+
+            timePickerDialog.show()
+        }
+        alarm2View.setOnClickListener {
+            // 現在の時間を取得
+            val calendar = Calendar.getInstance()
+
+            // TimePickerDialogを作成して表示
+            val timePickerDialog = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+                    notifyTime2Hour = hourOfDay
+                    notifyTime2Min = minute
+                    alarm2View.text = String.format("%02d:%02d", hourOfDay, minute)
+                }
+            }, notifyTime2Hour, notifyTime2Min, true) // trueは24時間形式、falseはAM/PM形式
+
+            timePickerDialog.show()
+        }
+        alarm3View.setOnClickListener {
+            // 現在の時間を取得
+            val calendar = Calendar.getInstance()
+
+            // TimePickerDialogを作成して表示
+            val timePickerDialog = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+                    notifyTime3Hour = hourOfDay
+                    notifyTime3Min = minute
+                    alarm3View.text = String.format("%02d:%02d", hourOfDay, minute)
+                }
+            }, notifyTime3Hour, notifyTime3Min, true) // trueは24時間形式、falseはAM/PM形式
+
+            timePickerDialog.show()
+        }
+
+        val btSave = findViewById<Button>(R.id.save_button)
+        val btSaveListener = SaveButtonListener()
+        btSave.setOnClickListener(btSaveListener)
+    }
+
+    private inner class SaveButtonListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            val alarm1Switch: SwitchCompat = findViewById(R.id.alarm1_switch)
+            DataManager.setNotifyTime1En(alarm1Switch.isChecked)
+            DataManager.setNotifyTime1Hour(notifyTime1Hour)
+            DataManager.setNotifyTime1Minute(notifyTime1Min)
+
+            val alarm2Switch: SwitchCompat = findViewById(R.id.alarm2_switch)
+            DataManager.setNotifyTime2En(alarm2Switch.isChecked)
+            DataManager.setNotifyTime2Hour(notifyTime2Hour)
+            DataManager.setNotifyTime2Minute(notifyTime2Min)
+
+            val alarm3Switch: SwitchCompat = findViewById(R.id.alarm3_switch)
+            DataManager.setNotifyTime3En(alarm3Switch.isChecked)
+            DataManager.setNotifyTime3Hour(notifyTime3Hour)
+            DataManager.setNotifyTime3Minute(notifyTime3Min)
+
+            DataManager.saveSettingData()
+        }
     }
 }
